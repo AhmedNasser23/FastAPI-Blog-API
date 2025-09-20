727f25249d93db2667939f2f312cce0495b71c14
AhmedNasser23 <ahmednasser233005@gmail.com>
2025-09-20 22:27:45 +0300

    Stopped At Docker

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
-from . import models
-from .database import engine
 from .routers import post, user, auth,vote
-from .config import settings
-
+from fastapi.middleware.cors import CORSMiddleware
 
 # Bind models to the database
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine) # We already have alembic
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(vote.router)
 app.include_router(post.router)
@@ -18,3 +27,4 @@ app.include_router(auth.router)
 @app.get("/")
 def root():
     return {"message": "Welcome to my API!"}
+
--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete='CASCADE'),nullable=False)
 
     owner = relationship("User")
 
@@ -28,6 +28,6 @@ class User(Base):
 class Vote(Base):
     __tablename__ = "votes"
 
-    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
-    post_id = Column(Integer, ForeignKey("posts.id"), primary_key=True) 
+    user_id = Column(Integer, ForeignKey("users.id", ondelete='CASCADE'), primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete='CASCADE'), primary_key=True) 
 
